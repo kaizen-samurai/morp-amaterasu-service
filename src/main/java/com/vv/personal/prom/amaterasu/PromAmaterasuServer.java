905c5cc293bf343c34c77871f001f6b4837f8200
Vivek <vivektheintel@gmail.com>
2020-12-23 14:51:58 +0530

    Base commit

---
+++ src/main/java/com/vv/personal/prom/amaterasu/PromAmaterasuServer.java
@@ -0,0 +1,34 @@
+package com.vv.personal.prom.amaterasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class PromAmaterasuServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromAmaterasuServer.class, args);
+	}
+
+	@Bean
+	ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+		return new ProtobufHttpMessageConverter();
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.vv.personal.prom.amaterasu"))
+				.build();
+	}
+}
